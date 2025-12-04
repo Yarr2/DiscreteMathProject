7e8d58eb3b55eac8711eac7740db28a841977c29
Yarr_laptop <yarema.stus@gmai>
2025-12-04 13:02:57 +0200

    basic functions for matrix represantation

--- graph(matrix)/matrix_graph.py
+++ graph(matrix)/matrix_graph.py
@@ -1,5 +1,6 @@
 class Graph:
-    def __init__(self, vertices : list, edges : list):
+    def __init__(self, vertices : list):
+        self.vertices = vertices
         self.number_of_vertices = len(vertices)
         self.matrix = []
         for _ in vertices:
@@ -7,3 +8,22 @@ class Graph:
             for _ in vertices:
                 temp.append(0)
             self.matrix.append(temp)
+
+    def add_edge(self,vertice_1,vertice_2) -> bool:
+        if not(vertice_1 in self.vertices and vertice_2 in self.vertices):
+            return False
+        self.matrix[vertice_1][vertice_2] = 1
+        self.matrix[vertice_2][vertice_1] = 1
+        return True
+
+    def remove_edge(self,vertice_1,vertice_2):
+        if not (vertice_1 in self.vertices and vertice_2 in self.vertices):
+            return False
+        self.matrix[vertice_1][vertice_2] = 0
+        self.matrix[vertice_2][vertice_1] = 0
+        return True
+
+    def add_vertice(self,vertice):
+        self.vertices.append(vertice)
+        self.number_of_vertices += 1
+
